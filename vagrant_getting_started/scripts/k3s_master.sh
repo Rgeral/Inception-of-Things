90bfbe62dd32782540d9c836c15927fddac7bc14
Rgeral <rgeral.pro@gmail.com>
2024-02-26 15:48:10 +0100

    working scripts

--- vagrant_getting_started/scripts/k3s_master.sh
+++ vagrant_getting_started/scripts/k3s_master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[K3S] : Installation en cours..."
-export K3S_INSTALL_OPTIONS="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1 --bind-address=$1 --advertise-address=$1"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1 --bind-address=$1 --advertise-address=$1"
 curl -sfL https://get.k3s.io | sh -
 
 echo "[K3S] : Copie du jeton du nœud maître vers (/vagrant/scripts/node-token)"
--- vagrant_getting_started/scripts/k3s_worker.sh
+++ vagrant_getting_started/scripts/k3s_worker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export TOKEN_FILE="/vagrant/scripts/node-token"
-export K3S_INSTALL_OPTIONS="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
+export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
 
 echo "Exécution de l'agent..."
-curl -sfL https://get.k3s.io | K3S_INSTALL_EXEC="$K3S_INSTALL_OPTIONS" sh -
+curl -sfL https://get.k3s.io | sh -
 
 echo "[SETUP] : Initialisation des alias pour tous les utilisateurs de la machine"
 echo "alias k='kubectl'" | sudo tee /etc/profile.d/00-aliases.sh > /dev/null
