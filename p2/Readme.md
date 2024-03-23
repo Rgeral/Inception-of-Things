c2f98c746b6026338ebe2820da5ba2bb761a0cc5
Rgeral <rgeral.pro@gmail.com>
2024-03-23 15:47:07 +0100

    part2 seems ok

---
+++ p2/Readme.md
@@ -0,0 +1,2 @@
+Don't forget to switch /etc/hosts 
+```192.168.56.110 app1.com app2.com app3.com```
--- p2/scripts/k3s_master.sh
+++ p2/scripts/k3s_master.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
-#documentation https://docs.k3s.io/installation/configuration#configuration-with-install-script
+# Documentation : https://docs.k3s.io/installation/configuration#configuration-with-install-script
 
+# Installation de K3S
 echo "[K3S] : Installation en cours..."
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1 --bind-address=$1 --advertise-address=$1"
-curl -sfL https://get.k3s.io | sh -
+if curl -sfL https://get.k3s.io | sh - > /dev/null; then
+    echo "[K3S] : Installation terminée avec succès"
+else
+    echo "[K3S] : Erreur lors de l'installation de K3S" >&2
+    exit 1
+fi
 
+# Copie du jeton du nœud maître
 echo "[K3S] : Copie du jeton du nœud maître vers (/vagrant/scripts/node-token)"
-# mkdir /usr/share/nginx
-# mkdir /usr/share/nginx/app1
-# mkdir /usr/share/nginx/app1/html
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/
 
-#APP 1
-sudo kubectl create configmap app1-html --from-file /vagrant/confs/app1/index.html
-sudo /usr/local/bin/kubectl apply -f /vagrant/confs/app1/app1.yaml
+# Installation des applications
 
+install_app() {
+    local app_name="$1"
+    local configmap_name="${app_name//-/_}"-html
+    local app_yaml="/vagrant/confs/${app_name}/${app_name}.yaml"
 
-#APP2
-sudo kubectl create configmap app2-html --from-file /vagrant/confs/app2/index.html
-sudo /usr/local/bin/kubectl apply -f /vagrant/confs/app2/app2.yaml
+    echo "[$app_name] : Installation de l'application $app_name..."
+    if sudo kubectl create configmap "$configmap_name" --from-file "/vagrant/confs/${app_name}/index.html" > /dev/null && \
+        sudo /usr/local/bin/kubectl apply -f "$app_yaml" > /dev/null; then
+        echo "[$app_name] : Succès"
+    else
+        echo "[$app_name] : Erreur lors de l'installation de l'application $app_name" >&2
+    fi
+}
 
-#APP3
+install_app "app1"
+install_app "app2"
+install_app "app3"
 
-sudo kubectl create configmap app3-html --from-file /vagrant/confs/app3/index.html
-sudo /usr/local/bin/kubectl apply -f /vagrant/confs/app3/app3.yaml
-
+# Configuration de la machine
 echo 'export PATH="/sbin:$PATH"' >> /home/vagrant/.bashrc
-
 source /home/vagrant/.bashrc
 
 echo "[SETUP] : Initialisation des alias pour tous les utilisateurs de la machine"
 echo "alias k='kubectl'" | sudo tee /etc/profile.d/00-aliases.sh > /dev/null
-
-
 
 echo "[Machine : $(hostname)] a été configurée avec succès!"
