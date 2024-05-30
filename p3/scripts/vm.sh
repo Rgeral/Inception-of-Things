b1259bd074b7c8ee719fc2d43d36022b34ef860d
rgeral <geral.robin@yahoo.fr>
2024-05-30 17:18:12 +0200

    seems working

--- p3/scripts/vm.sh
+++ p3/scripts/vm.sh
@@ -1,9 +1,38 @@
-VBoxManage createvm --name debian-iot --ostype Debian_64 --register
-vboxmanage modifyvm debian-iot --cpus 4 --memory 4096 --vram 12
-VBoxManage storagectl "debian-iot" --name "SATA" --add sata
-VBoxManage storageattach "debian-iot" --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium /home/rgeral/data/debian-12.5.0-amd64-netinst.iso
-VBoxManage unattended install "debian-iot" --iso="/home/rgeral/data/debian-12.5.0-amd64-netinst.iso" --user=rgeral --password=1234 --full-user-name="rgeral" --country=US --time-zone=UTC
-VBoxManage createhd --filename "/home/rgeral/data/debian-iot.vdi" --size 20480
-VBoxManage storageattach "debian-iot" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "/home/rgeral/data/debian-iot.vdi"
-vboxmanage storageattach "debian-iot" --storagectl "SATA" --port 2 --device 0 --type dvddrive --medium b8bee39c-473a-4002-a7d5-a8c52eda2932
-VBoxManage startvm "debian-iot" --type headless
+#!/bin/bash
+
+# basic config
+sudo -i
+apt-get update
+
+# docker
+apt-get -y install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# alias k for kubectl
+echo "[LOG] - Alias for kubectl"
+echo "alias k=kubectl" >> /etc/bash.bashrc
+source /etc/bash.bashrc
+
+# k3d
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d cluster create kbarbry
+
+# argocd
+kubectl create namespace argocd
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl port-forward -n argocd svc/argocd-server 8080:443
